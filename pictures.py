88f11a9f9402ac84c15dbdbe5ed4e8d29887d14c
Greg Baker <gregb@ifost.org.au>
2016-09-06 09:25:38 +1000

    Initial framework.

---
+++ pictures.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import skimage.io
+import matplotlib.pyplot
+import os
+
+######################################################################
+# bus
+def display_bus():
+    if os.path.exists('bus.jpg'):
+        bus = skimage.io.imread('bus.jpg')
+        matplotlib.pyplot.imshow(bus)
+
+######################################################################
+# unicorns
+def display_unicorn():
+    pass
+
+######################################################################
+# platypus
+def display_platypus():
+    pass
+
+
+######################################################################
+# dragon
+def display_dragon():
+    pass
+
+######################################################################
+# beach scene
+def display_beach_scene():
+    pass
+
+
+######################################################################
+# sky
+def display_sky():
+    pass
+
+
+######################################################################
+# Main program
+
+display_bus()
+display_unicorn()
+display_platypus()
+display_dragon()
+display_beach_scene()
+display_sky()
